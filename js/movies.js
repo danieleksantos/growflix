0bd8eecad67ae44ea96c2b91a1a07ffdcc4235b7
Daniele Karina dos Santos <danimistron@gmail.com>
2025-07-08 15:11:55 -0300

    start project

---
+++ js/movies.js
@@ -0,0 +1,10 @@
+const movies = [
+{
+img: "https://img.youtube.com/vi/<id>/sddefault.jpg",
+title: "Título do vídeo",
+link: "https://www.youtube.com/embed/<id>",
+category: "Categoria do vídeo",
+featuread: true,
+},
+
+];
